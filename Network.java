b0b3d8b70a6ddde6e835efda9b1b012552784449
Keren Gantman <keren.gantman@post.runi.ac.il>
2026-01-09 18:35:31 +0200

    finish1

--- Network.java
+++ Network.java
@@ -70,6 +70,9 @@ public class Network {
      * or if the "follows" addition failed for some reason, returns false.
      */
     public boolean addFollowee(String name1, String name2) {
+        if (name1.equals(name2)) { // ADD THIS CHECK
+            return false;
+        }
         User user1 = getUser(name1);
         User user2 = getUser(name2);
         if (user1 == null || user2 == null) {
